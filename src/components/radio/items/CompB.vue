1816560888ca4ed8d63c963684210c4298e01fcf
takashi.tsuji <takashi.tsuji@icloud.com>
2024-10-25 10:31:55 +0900

    feat: append watch & localstorage codes

--- src/components/radio/items/CompB.vue
+++ src/components/radio/items/CompB.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive, onMounted, watch } from 'vue'
 
 defineProps({
-  msg: String
+    msg: String
 })
 
-const value = ref('')
+const data = reactive({
+    text: '',
+    count: 0
+});
+
+onMounted(() => {
+    const storedCompb = localStorage.getItem('compb');
+    if (storedCompb) {
+        Object.assign(data, JSON.parse(storedCompb));
+    }
+    // console.log('onMounted: ', data);
+
+});
+
+watch(data, (newVal) => {
+    localStorage.setItem('compb', JSON.stringify(newVal));
+    // console.log('localStorage.setItem: ', newVal);
+}, { deep: true });
 
 </script>
 
@@ -16,15 +33,23 @@ const value = ref('')
 
         <div class="py-5 w-1/3">
             <div>
-                <span>Message is: {{ value }}</span>
+                <span>Message is: {{ data.text }}</span>
             </div>
             <div>
                 <input 
                     class="border focus:border-2"
-                    v-model="value"
+                    v-model="data.text"
                     placeholder="type input ... "
                 />
             </div>
+            <div>
+                <span>count: {{ data.count }}</span>
+            </div>
+            <div class="space-x-1">
+                <button type="button" class="bg-blue-400 hover:bg-blue-300 text-white rounded w-10 h-10" @click="data.count++">+</button>
+                <button type="button" class="bg-orange-400 hover:bg-orange-300 text-white rounded w-10 h-10" @click="data.count--">-</button>
+            </div>
+
         </div>
     </div>
 </template>
